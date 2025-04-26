8d18c3f689511309d61a95aa50a8102207333324
Qwame Palmer <qwamepalmer@gmail.com>
2025-04-25 21:40:49 -0500

    saving because I didn't adhere to the save early save often rule, ended up going in circles # Please enter the commit message for your changes. Lines starting

---
+++ script.js
@@ -0,0 +1,39 @@
+//initialize buttons
+const resizeButton = document.querySelector('.resize')
+const clearButton = document.querySelector('.clear')
+const blackButton = document.querySelector('.black')
+const rgbButton = document.querySelector('.rgb')
+
+//button event listners
+
+
+//start functions
+
+
+
+
+//generate the grid
+
+
+  
+  
+const grid =  document.querySelector('.grid');
+
+for (let i = 0; i < 256; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  grid.appendChild(square); }
+
+
+
+
+
+/*update grid
+const updateGrid = (userInput) => {
+  let userInput = prompt("Ener Size of grid");
+  gridbox
+
+
+}
+
+*/
